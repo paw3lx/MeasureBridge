dd1085d303d15de78bb1b90159bdae46625d961d
Pawel Skaruz <pask@netcompany.com>
2019-08-06 19:37:54 +0200

    Add project files.

---
+++ MeasureBridge.AzureSphere/main.c
@@ -0,0 +1,15 @@
+﻿#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+
+#include <applibs/log.h>
+#include <applibs/gpio.h>
+
+int main(void)
+{
+    const struct timespec sleepTime = {1, 0};
+    while (true) {
+        nanosleep(&sleepTime, NULL);
+    }
+}
